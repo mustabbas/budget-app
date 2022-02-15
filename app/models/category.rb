d58bd622497625357741edd2d3249729027d8908
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-02-16 00:16:07 +0300

    Setup test, Add model validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :category_transacs, dependent: :delete_all
   has_many :transacs, through: :category_transacs
+  validates :name, presence: true
 
   def amount
     result = 0
